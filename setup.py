9240cf71d299c9601864f55943cd2d8f8f4e744f
pc <pc@debian202.udman.ru>
2013-12-10 15:48:15 +0400

    Добавлен установщик

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wlcomp',
+    version="0.1",
+    packages=find_packages(),
+    description="Python CFFI port for LComp library",
+)
